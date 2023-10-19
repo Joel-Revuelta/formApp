b4e34c7b372b19e08317e67b29fdc9f5c31db835
Joel <joel.revuel@gmail.com>
2023-10-19 12:44:57 +0200

    complete formApp

---
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
+
+@Component({
+  templateUrl: './switches-page.component.html'
+})
+export class SwitchesPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) {}
+
+  isValidField(field: string): boolean {
+    return this.validatorsService.isValidField( this.myForm, field );
+  }
+
+  onSave() {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+  
+    console.log( this.myForm.value );
+    console.log(this.person);
+
+    this.myForm.reset({gender: 'M', wantNotifications: true, termsAndConditions: false});
+
+  }
+
+}
